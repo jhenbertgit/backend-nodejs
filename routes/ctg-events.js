e1b17c562c63ac9c595c7488c54dc8446f4f6a1c
Jhenbert Villamucho <jvv205756@gmail.com>
2023-11-14 23:13:07 +0800

    minor commit ctg-events

--- routes/ctg-events.js
+++ routes/ctg-events.js
@@ -7,6 +7,6 @@ router.get("/", db.getAllEvents);
 router.get("/:id", db.getEventsById);
 router.post("/", db.create);
 router.put("/:id", db.update);
-router.delete("./:id", db.remove);
+router.delete("/:id", db.remove);
 
 module.exports = router;
